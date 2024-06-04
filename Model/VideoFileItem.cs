961bc295438d6158e5d90c4c2f971b38242add8e
AKRITI612 <32239748+AKRITI612@users.noreply.github.com>
2024-06-04 21:19:01 +0530

    Add files via upload

---
+++ Model/VideoFileItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace VideoPlayerApplication.Model
+{
+   
+    [Serializable]
+    public class VideoFileItem
+    {        
+        #pragma warning disable CS8618 
+
+        public VideoFileItem()
+        {
+
+        }
+
+        public VideoFileItem(string VideoName, string VideoType,string VideoLocation,string VideoSize)
+        {
+            this.VideoName = VideoName;
+            this.VideoType = VideoType;
+            this.VideoLocation = VideoLocation;
+            this.VideoSize = VideoSize;
+        }
+
+        /// <summary>
+        /// Video Name 
+        /// </summary>
+        public string VideoName { get; set; }
+
+        /// <summary>
+        /// Video Type
+        /// </summary>
+        public string VideoType { get; set; }
+
+        /// <summary>
+        /// Video Location
+        /// </summary>
+        public string VideoLocation { get; set; }
+
+        /// <summary>
+        /// Video Size
+        /// </summary>
+        public string VideoSize { get; set; }
+    }
+}
---
+++ Model/VideoFileMgr.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace VideoPlayerApplication.Model
+{
+
+#pragma warning disable CS8618
+
+    public class VideoFileMgr
+    {
+        private readonly string _videoTitle = "Media Player";      
+
+        private static VideoFileMgr _mInst;
+        public static VideoFileMgr Instance
+        {
+            get => _mInst ??= new VideoFileMgr();
+
+            private set => _mInst = value;
+        }
+
+        public string VideoTitle { get ; set ; }
+        public string ConfigFilePath { get ; set ; }
+        public bool IsVideOpen { get ; set ; }
+        public List<string> SupportedFiles { get; set; }
+        public VideoFileItem SelectedVideoItem 
+        { 
+          get;
+          set ; 
+        }
+        public ObservableCollection<VideoFileItem> Favouriteslist { get ; set ; }
+
+        private VideoFileMgr()
+        {           
+            VideoTitle = _videoTitle;
+            IsVideOpen = false;
+            Favouriteslist = new ObservableCollection<VideoFileItem>();
+            SupportedFiles = new List<string>();
+        }
+
+        /// <summary>
+        ///  Set the supported File Types.
+        /// </summary>
+        /// <param name="pAllOptions"></param>
+        public void SetVideoPlayerInfo(MediaPlayerInfo pAllOptions)
+        {
+            SupportedFiles = pAllOptions.SupportedVideoFileTypes.Split(',').ToList();
+        }
+    }
+}
