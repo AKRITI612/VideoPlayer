38d1ad4d64ff689642b0ab9b6562dc0bd2a13ca7
AKRITI612 <32239748+AKRITI612@users.noreply.github.com>
2024-06-04 21:35:44 +0530

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using VideoPlayerApplication.Model;
+using VideoPlayerApplication.Service;
+
+namespace VideoPlayerApplication
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string fileName = AppDomain.CurrentDomain.BaseDirectory + @"\" + "UserConfig.xml";
+
+            //Load application settings.
+            MediaPlayerSettings.settingsFilePath = AppDomain.CurrentDomain.BaseDirectory + "VideoPlayerSettings.xml";
+            MediaPlayerSettings.LoadMediaPlayerSettings();
+
+            //Set the Favourites Config file path
+            VideoFileMgr.Instance.ConfigFilePath = fileName;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using VideoPlayerApplication.Service;
+using VideoPlayerApplication.ViewModel;
+
+namespace VideoPlayerApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {           
+            InitializeComponent();
+            MainWindowViewModel? vm = DataContext as MainWindowViewModel;
+            vm.DialogService = new FileDialogService();
+            vm.VideoCollectionService = new VideoCollectionService();
+            vm.ShowDialog = new ShowWindowDialog();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is MainWindowViewModel vm)
+            {
+                vm.LoadSettings();
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Close();
+            if (DataContext is MainWindowViewModel vm)
+            {
+                vm.SaveSettings();
+            }
+            MediaPlayerSettings.SaveMediaPlayerSettings();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateLayout();
+        }
+    }
+}
