0e955b726662de5c78eae7767da42082109e6841
AKRITI612 <32239748+AKRITI612@users.noreply.github.com>
2024-06-04 21:30:14 +0530

    Add files via upload

---
+++ View/DownloadVideoControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using VideoPlayerApplication.ViewModel;
+
+namespace VideoPlayerApplication.View
+{
+    /// <summary>
+    /// Interaction logic for DownloadVideoControl.xaml
+    /// </summary>
+    public partial class DownloadVideoControl : Window
+    {
+        public DownloadVideoControl()
+        {
+            InitializeComponent();
+            DownloadVideoControlViewModel obj = new DownloadVideoControlViewModel();
+            obj.RequestClose += Obj_RequestClose;
+            DataContext = obj;
+        }
+
+        private void Obj_RequestClose()
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void DownloadDialog_Closed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ View/NoMediaPlayerControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace VideoPlayerApplication.View
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class NoMediaPlayerControl : UserControl
+    {
+        public NoMediaPlayerControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
