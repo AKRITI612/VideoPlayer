f1a42ad1ccc9532f27fbaf0c9e386e43a35ee869
AKRITI612 <32239748+AKRITI612@users.noreply.github.com>
2024-06-04 21:30:56 +0530

    Add files via upload

---
+++ View/CommonGUI/WpfXamlConverters.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace VideoPlayerApplication.View.CommonGUI
+{
+    /// <summary>
+    /// Inverse to Boolean converter.
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new Exception();
+        }
+    }
+
+    /// <summary>
+    /// Boolean To Visibility Converter
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                if (value != null)
+                {
+                    if ((bool)value)
+                        return Visibility.Visible;
+                    else
+                        return Visibility.Hidden;
+                }
+                else
+                {
+                    return Visibility.Hidden;
+                }
+            }
+            else
+            {
+                return Visibility.Hidden;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
